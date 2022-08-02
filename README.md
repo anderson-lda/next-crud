43e69b23b25a500453dedb88747357deab9e1bbd
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-08-02 13:55:05 -0300

    table component done

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 https://tailwindcss.com/docs/guides/nextjs
  - Após criação do tsconfig.json (preenchido após o próximo nom run dev), foi executado o comando: 
 npm install --save-dev typescript @types/react @types/node typescript
-obs: adicionado manualmente no tsconfig: "moduleResolution": "node", e atualizado para ES2015 para se usar anotações # em atributos
+obs: adicionado manualmente no tsconfig: "moduleResolution": "node", e atualizado para ES2015
+ - ícones obtidos em:
+ https://heroicons.com/
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana',34,'1'),
+    new Cliente('Zeca',45,'2'),
+    new Cliente('Maria',25,'3'),
+  ]
+
+  function clienteSelecionado(cliente:Cliente){
+  }
+
+  function clienteExcluido(cliente:Cliente){
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +23,11 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela 
+          clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout>
     </div>
   )
