340a9b619d5acd2dd5759db3b0f817a320e7e5b1
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-08-01 23:57:28 -0300

    initial configurations done

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='bg-gradient-to-r from-purple-500 to-blue-600'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
